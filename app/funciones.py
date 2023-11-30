e67c997703671ba3628d808a751f62d07c4d15aa
Diego Núñez González <diego.dng.1994@gmail.com>
2023-11-30 04:38:28 +0100

    Version Final

--- app/funciones.py
+++ app/funciones.py
@@ -3,23 +3,17 @@ import joblib
 import streamlit as st
 
 def predecir_precio(make, year, doors, kms, power):
-    # antiquity = 2023 - year
-    modelo = joblib.load('../models/modelo_KNeighborsRegressor.pkl')
-    df_pred = pd.read_csv('../data/test/coches_test.csv')
-    df_pred = df_pred.drop(df_pred.index)
-    # x = df[["kms", "power", "antiquity", "doors"]]
+   
+    modelo = joblib.load('../models/modelo_RandomForestRegressor/modelo_RandomForestRegressor.pkl')
+    
     antiquity = 2023 - year
     nuevo_registro = {'antiquity': antiquity,'doors': doors, 'kms': kms, "power": power}
+    df_pred = pd.DataFrame([nuevo_registro])
 
-    # Añadir el nuevo registro al DataFrame
-    df_pred = pd.concat([df_pred, pd.DataFrame([nuevo_registro])], ignore_index=True)
-    df_pred = df_pred.dropna(axis=1)
-    #df_pred
-    # Preprocesamiento de datos (ajusta esto según el preprocesamiento real que hiciste durante el entrenamiento)
-    #datos = pd.DataFrame({'Marca': [marca], 'Año': [año], 'Kilometraje': [kilometraje], 'Potencia': [potencia]})
-        
-    # Realizar la predicción utilizando el modelo cargado
+    print(df_pred)
+    
     precio_predicho = modelo.predict(df_pred)
+    print("el precio es:", precio_predicho)
     precio_predicho = precio_predicho.round(2)
     precio_predicho = str(precio_predicho[0]) + "€"
     precio_predicho
@@ -27,21 +21,18 @@ def predecir_precio(make, year, doors, kms, power):
     return precio_predicho
 
 def formulario():
-    st.title("Formulario y Predicción de Precio de Automóviles")
+    
 
-    # Recopilar información del usuario
     make = st.text_input("Marca del vehículo:")
     year = st.number_input("Año del vehículo:", max_value=2023, step=1)
     doors = st.number_input("Puertas del vehículo:")
     kms = st.number_input("Kilometraje del vehículo:")
     power = st.number_input("Potencia del vehículo:")
 
-    # Botón para realizar la predicción
+
     if st.button("Predecir Precio"):
-        # Realizar la predicción utilizando la función predefinida
         precio_predicho = predecir_precio(make, year, doors, kms, power)
 
-        # Mostrar la información recopilada y la predicción
         st.success("Información Recopilada:")
         st.info(f"Marca: {make}")
         st.info(f"Año: {year}")
--- app/streamlit.py
+++ app/streamlit.py
@@ -13,10 +13,9 @@ import joblib
 import funciones
 
 # Título de la página
-st.set_page_config(page_title = 'Valor coche', page_icon= ":car:")
+st.set_page_config(page_title = 'valorDeTuCoche.net', page_icon= ":car:")
+st.image('img/concesionario.jpg')
 st.title('Valor de un coche:')
-st.header('Una predicción realizada con Machine Learning')
-#st.image('https://sepuedeonosepuede.com/wp-content/uploads/2022/10/comer-granos-de-cafe-scaled.jpg')
 st.divider()
 
 # Datasets a trabajar
@@ -25,24 +24,8 @@ coches_processed = pd.read_csv('../data/processed/coches_processed.csv')
 coches_train = pd.read_csv('../data/train/coches_train.csv')
 cohes_test = pd.read_csv('../data/test/coches_test.csv')
 
-st.sidebar.title('valorDeTuCoche.net')
-st.sidebar.divider()
-# ¿Agua Sucia o Café
-if st.sidebar.button('Inicio.'):
-    st.title('Descripción del modelo.')
-    st.markdown('Con este modelo podremos predecir el valor de un coche en función de sus caracteristicas.')
-    
-if st.sidebar.button('Datos.'):
-    st.title('Aqui meto los datos')
-    st.markdown('en esta zona se muestran los datos')
-
-if st.sidebar.button('Modelo.'):
-    st.title('Ejemplo de modelo')
-    st.markdown('Meter una tabla con las prediciones del modelo.')
-
-if st.sidebar.button('Pon a prueba nuestro modelo.'):
-    st.title('Introduce los datos del coche:')
-    
 funciones.formulario()  
+    
+
 
 
